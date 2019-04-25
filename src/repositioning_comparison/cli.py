f62facbbd297e32d6a3e496797c1d792ecb8fe4e
lingling93 <763704059@qq.com>
2019-04-25 16:46:13 +0200

    Add results folder

--- src/repositioning_comparison/cli.py
+++ src/repositioning_comparison/cli.py
@@ -1,38 +1,77 @@
+import os
+
 import click
+import simplejson
+from joblib import dump, load
 
 from .create_graph import create_himmelstein_graph
 from .download import ensure_data, DIRECTORY
 from .nodetovec import nodetovec, HadamardEmbedder, AverageEmbedder, WeightedL1Embedder, WeightedL2Embedder
+from .pairs import train_pairs, test_pairs
 from .subgraph import subgraph
 from .train import train, validate
-from .pairs import train_pairs, test_pairs
 
 GRAPH_TYPES = ['subgraph', 'wholegraph', 'permutation1', 'permutation2', 'permutation3', 'permutation4', 'permutation5']
 EMBEDDER_TYPES = ['HadamardEmbedder', 'AverageEmbedder', 'WeightedL1Embedder', 'WeightedL2Embedder']
-
+HERE = os.path.abspath(os.path.dirname(__file__))
+DEFAULT_DIRECTORY = os.path.abspath(os.path.join(HERE, os.pardir, os.pardir, 'results'))
+RESULTS_DIRECTORY = os.environ.get('REPOSITIONING_COMPARISON_DIRECTORY', DEFAULT_DIRECTORY)
 
 @click.command()
 @click.argument('graphtype', type=click.Choice(GRAPH_TYPES))
 @click.option('--data-directory', type=click.Path(dir_okay=True, file_okay=False), default=DIRECTORY, show_default=True)
+@click.option('-d', '--output-directory', type=click.Path(dir_okay=True, file_okay=False), default=RESULTS_DIRECTORY,
+              help='Output directory. Defaults to current working directory.')
 @click.option('--method', default='node2vec', type=click.Choice(['node2vec', 'edge2vec', 'metapath2vec']))
 @click.option('--embedder', default='HadamardEmbedder', type=click.Choice(EMBEDDER_TYPES))
-def main(graphtype, data_directory, method, embedder):
+def main(graphtype, data_directory, output_directory, method, embedder):
     """This cli runs the ComparisonNRL."""
-    nodepath, edgepath, featurepath, validatepath = ensure_data(directory=data_directory)
+    nodepath, edgepath, featurepath, validatepath, permutation_paths = ensure_data(directory=data_directory)
 
     if graphtype == 'subgraph' and method == 'node2vec':
-        result = subgraph_node2vec(nodepath, edgepath, featurepath, embedder)
+        result = subgraph_node2vec(nodepath, edgepath, featurepath, embedder,output_directory)
         click.echo(f'Result: {result}')
+
     elif graphtype == 'wholegraph' and method == 'node2vec':
         wholegraph = create_himmelstein_graph(nodepath,edgepath)
         result = graph_node2vec(wholegraph,nodepath, edgepath, featurepath,validatepath, embedder)
         click.echo(f'Result: {result}')
 
 
-def subgraph_node2vec(nodepath, edgepath, featurepath, embedder):
+def subgraph_node2vec(nodepath, edgepath, featurepath, embedder,output_directory):
     biggraph = create_himmelstein_graph(nodepath, edgepath)
-    thisgraph, positive_list, positive_label, negative_list, negative_label = subgraph(featurepath, biggraph, 3, 10,
-                                                                                       20)
+    thisgraph, positive_list, positive_label, negative_list, negative_label = subgraph(featurepath, biggraph, 3, 10,20)
+    dir_path =  os.path.join (output_directory,'subgraph_node2vec')
+    if not os.path.exists(dir_path):
+        os.mkdir(dir_path)
+
+    metadata_file = os.path.join(dir_path,'metadata')
+    if not os.path.exists(metadata_file):
+        metadata_file = open(metadata_file,'w')
+    metadata_file = open(metadata_file,'w')
+    metadata_file.write('embedder: '+embedder+ '\n')
+    metadata_file.close()
+
+    train_file = os.path.join(dir_path,'train')
+    if not os.path.exists(train_file):
+        train_file = open(train_file,'w')
+    train_file = open(train_file,'w')
+
+    test_file = os.path.join(dir_path,'test')
+    if not os.path.exists(test_file):
+        test_file = open(test_file,'w')
+    test_file = open(test_file,'w')
+
+    model_file = os.path.join(dir_path,'model')
+    if not os.path.exists(model_file):
+        model_file = open(model_file,'wb')
+    model_file = open(model_file,'w')
+
+    validate_file = os.path.join(dir_path,'validate')
+    if not os.path.exists(model_file):
+        validate_file = open(validate_file,'w')
+    validate_file = open(validate_file,'w')
+
     model = nodetovec(thisgraph)
     if embedder == 'HadamardEmbedder':
         positive_vecs = HadamardEmbedder(model, positive_list)
@@ -51,11 +90,22 @@ def subgraph_node2vec(nodepath, edgepath, featurepath, embedder):
 
     train_vecs = positive_vecs[0:5] + negative_vecs[0:15]
     train_labels = positive_label[0:5] + negative_label[0:15]
+    train_data = [train_vecs,train_labels]
+    simplejson.dump(train_data, train_file)
+    train_file.close()
+
     test_vecs = positive_vecs[5:] + negative_vecs[15:]
     test_labels = positive_label[5:] + negative_label[15:]
+    test_data = [test_vecs , test_labels]
+    simplejson.dump(test_data,test_file)
+    test_file.close()
+
     lg = train(train_vecs, train_labels)
+    dump(lg, model_file)
     roc = validate(lg, test_vecs, test_labels)
-    return roc
+    validate_file.write(roc)
+
+
 
 
 def graph_node2vec(graph,nodepath, edgepath, featurepath,validatepath, embedder):
--- src/repositioning_comparison/download.py
+++ src/repositioning_comparison/download.py
@@ -2,7 +2,7 @@
 
 import logging
 import os
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 from urllib.request import urlretrieve
 
 logger = logging.getLogger(__name__)
@@ -16,9 +16,17 @@ NODE_DATA_URL = 'https://raw.githubusercontent.com/dhimmel/integrate/master/data
 EDGE_DATA_URL = 'https://raw.githubusercontent.com/dhimmel/integrate/master/data/edges.sif.gz'
 TRANSFORMED_FEATURES_URL = 'https://github.com/dhimmel/learn/blob/master/prediction/features/transformed-features.tsv.bz2?raw=true'
 VALIDATE_DATA_URL = 'https://github.com/dhimmel/learn/blob/master/validate/validation-statuses.tsv'
+PERMUTATION1_DATA_URL = 'https://github.com/dhimmel/integrate/blob/master/data/permuted/hetnet_perm-1.json.bz2'
+PERMUTATION2_DATA_URL = 'https://github.com/dhimmel/integrate/blob/master/data/permuted/hetnet_perm-2.json.bz2'
+PERMUTATION3_DATA_URL = 'https://github.com/dhimmel/integrate/blob/master/data/permuted/hetnet_perm-3.json.bz2'
+PERMUTATION4_DATA_URL = 'https://github.com/dhimmel/integrate/blob/master/data/permuted/hetnet_perm-4.json.bz2'
+PERMUTATION5_DATA_URL = 'https://github.com/dhimmel/integrate/blob/master/data/permuted/hetnet_perm-5.json.bz2'
+
+PERMUTATION_DATA_FILE_FMT = 'hetnet_perm-{}.json.bz2'
+PERMUTATION_DATA_URL_FMT = 'https://github.com/dhimmel/integrate/blob/master/data/permuted/hetnet_perm-{}.json.bz2'
 
 
-def ensure_data(directory: Optional[str] = None) -> Tuple[str, str, str]:
+def ensure_data(directory: Optional[str] = None) -> Tuple[str, str, str, str, List[str]]:
     """Ensure Himmelstein's data files are downloaded."""
     if directory is None:
         directory = DIRECTORY
@@ -42,8 +50,19 @@ def ensure_data(directory: Optional[str] = None) -> Tuple[str, str, str]:
 
     VALIDATE_DATA_PATH = os.path.join(directory, 'validation-statuses.tsv')
     if not os.path.exists(VALIDATE_DATA_PATH):
-        logger.warning(f'downloading {VALIDATE_DATA_PATH}')
+        logger.warning(f'downloading {VALIDATE_DATA_URL}')
         urlretrieve(VALIDATE_DATA_URL, VALIDATE_DATA_PATH)
 
+    PERMUTATION_DIRECTORY = os.path.join(directory, "permutations")
+    os.makedirs(PERMUTATION_DIRECTORY, exist_ok=True)
 
-    return NODE_DATA_PATH, EDGE_DATA_PATH, TRANSFORMED_FEATURES_PATH, VALIDATE_DATA_PATH
+    permutation_paths = []
+    for i in range(5):
+        permutation_data_path = os.path.join(PERMUTATION_DIRECTORY, PERMUTATION_DATA_FILE_FMT.format(i + 1))
+        if not os.path.exists(permutation_data_path):
+            url = PERMUTATION_DATA_URL_FMT.format(i + 1)
+            logger.warning(f'downloading {url}')
+            urlretrieve(url, permutation_data_path)
+        permutation_paths.append(permutation_data_path)
+
+    return NODE_DATA_PATH, EDGE_DATA_PATH, TRANSFORMED_FEATURES_PATH, VALIDATE_DATA_PATH, permutation_paths
