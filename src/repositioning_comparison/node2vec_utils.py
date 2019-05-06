8362320dbeeaf49d42908c4c58e628e672ca32f7
lingling93 <763704059@qq.com>
2019-05-06 11:23:00 +0200

    Change workers to 1

--- src/repositioning_comparison/node2vec_utils.py
+++ src/repositioning_comparison/node2vec_utils.py
@@ -10,18 +10,18 @@ import node2vec
 logger = logging.getLogger(__name__)
 
 
-def fit_node2vec(graph: networkx.Graph, workers: int = -1) -> gensim.models.Word2Vec:
+def fit_node2vec(graph: networkx.Graph, workers: int = 1) -> gensim.models.Word2Vec:
     """
 
     :param graph:
     :param workers: The number of workers to use. If -1, uses all available CPUs
     """
-    if workers == 0:
-        raise ValueError(f'Invalid number of workers: {workers}')
-    elif workers == -1:
-        workers = multiprocessing.cpu_count()
-    elif workers < -1:
-        workers = multiprocessing.cpu_count() + workers + 1
+    #if workers == 0:
+    #    raise ValueError(f'Invalid number of workers: {workers}')
+    #elif workers == -1:
+    #    workers = multiprocessing.cpu_count()
+    #elif workers < -1:
+    #    workers = multiprocessing.cpu_count() + workers + 1
 
     logger.info(f"Initializing Node2Vec with {workers}")
 
