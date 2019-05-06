1063c7679d941f98836b13b7fb36f35d338b983c
lingling93 <763704059@qq.com>
2019-05-06 16:11:42 +0200

    Separate validation datasets

--- src/repositioning_comparison/pairs.py
+++ src/repositioning_comparison/pairs.py
@@ -17,15 +17,33 @@ def train_pairs(path):
     return train_list, train_label
 
 
-def test_pairs(path):
-    df_validate = pd.read_csv(path, sep='\t')
-    test_list = []
-    test_label = []
+def test_pairs(validationpath, trainpath):
+    df_validate = pd.read_csv(validationpath, sep='\t')
+    diseaseModifying_pairs, diseaseModifying_labels = train_pairs(trainpath)
+    diseaseModifying =[diseaseModifying_pairs,diseaseModifying_labels ]
+    clinicalTrials = [[],[]]
+    drugCentral = [[],[]]
     for _, row in df_validate[
         ['compound_id', 'disease_id', 'n_trials', 'status_trials', 'status_drugcentral']].iterrows():
-        test_list.append(['Compound::' + row['compound_id'], 'Disease::' + row['disease_id']])
-        if row['n_trials'] or row['status_trials'] or row['status_drugcentral'] == 1:
-            test_label.append(1)
-        else:
-            test_label.append(0)
+        if row['status_trials'] ==1:
+            clinicalTrials[0].append(['Compound::' + row['compound_id'], 'Disease::' + row['disease_id']])
+            clinicalTrials[1].append(1)
+        if row['status_drugcentral'] == 1:
+            drugCentral[0].append(['Compound::' + row['compound_id'], 'Disease::' + row['disease_id']])
+            drugCentral[1].append(1)
+    return diseaseModifying, clinicalTrials, drugCentral
+
+
+
+
+
+
+
+
+
+
+
+
+
     return test_list, test_label
+
